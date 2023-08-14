4b6c1734cb66b863e72ee31e3a873b5b4c5ee240
susysd <susyane.dantas33@gmail.com>
2023-08-14 14:23:26 -0300

    Conhecendo o Blade

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $nome = "Matheus";
+    $idade = 29;
+   
+    return view('welcome', ['nome' => $nome, 'idade' => $idade, 'profissao' => "Programador"]);
 });
 
 Route::get('/contact', function () {
