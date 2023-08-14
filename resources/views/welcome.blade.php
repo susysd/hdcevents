f978ca623bd6f0aaf0281599e3c1d9240dd1e4a9
susysd <susyane.dantas33@gmail.com>
2023-08-14 15:31:43 -0300

    continuação sobre propriedades do blade/php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,5 +31,27 @@
        <p>O nome não é Pedro</p>
        @endif
 
+
+       @for($i = 0; $i < count($arr); $i++)
+       <p> {{ $arr[$i]}} - {{$i}} </p>
+       @if($i ==2)
+       <p>o i é 2 </p>
+       @endif
+       @endfor
+
+       @foreach ($nomes as $nome)
+       <p>{{$loop->index}}</p>
+       <p> {{ $nome }}</p>
+       @endforeach
+
+
+@php 
+$name ="João";
+echo $name;
+@endphp
+
+<!--Cometário do HTML -->
+{{-- Este é o comentário do Blade --}}
+
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,18 @@ Route::get('/', function () {
     $nome = "Matheus";
     $idade = 29;
    
-    return view('welcome', ['nome' => $nome, 'idade' => $idade, 'profissao' => "Programador"]);
+    $arr = [10,20,30,40,50];
+    
+    $nomes = ["Matheus", "Maria", "João", "Saulo"];
+
+    return view('welcome',
+     [
+        'nome' => $nome, 
+        'idade' => $idade,
+        'profissao' => "Programador",
+        'arr' => $arr,
+        'nomes' => $nomes
+     ]);
 });
 
 Route::get('/contact', function () {
