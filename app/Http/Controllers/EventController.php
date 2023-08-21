7d58a0b8e4b42aa3b6b8e3dca1f5ad02565e87ae
susysd <susyane.dantas33@gmail.com>
2023-08-21 15:47:58 -0300

    Upload deimagens com Laravel

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -26,9 +26,22 @@ class EventController extends Controller
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
+//Image Upload
+       
+        
+        if($request->hasFile('image') && $request->file('image')->isValid()){
 
+            $requestImage = $request->image;
+
+            $extension = $requestImage->extension();
+
+            $imageName = md5($request->image->getClientOriginalName() . strtotime("now"). "." . $extension);
+
+            $requestImage->move(public_path('img/events'), $imageName);
+
+            $event->image = $imageName;
+        }
         $event->save();
-
         return redirect('/')->with('msg', 'Evento criado com sucesso!');
     }
 
