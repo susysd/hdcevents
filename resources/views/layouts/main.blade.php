1b99bf4428ae8ec7efe4b92433adccf657745923
susysd <susyane.dantas33@gmail.com>
2023-08-22 16:56:32 -0300

    Autenticação Laravel

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -30,12 +30,32 @@
                     <li class="nav-item">
                         <a href="/events/create" class="nav-link">Criar Eventos</a>
                     </li>
+                    @auth
                     <li class="nav-item">
-                        <a href="/" class="nav-link">Entrar</a>
+                        <a href="/dashboard" class="nav-link">Meus Eventos</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <form action="/logout" method="POST">
+                            @csrf\
+                            <a href="/logout"
+                            class="nav-link"
+                            onClick="event.preventDefault();
+                            this.closest('form').submit();">
+                        Sair
+                        </a>
+                        </form>
+                    </li>
+                    @endauth
+
+                    @guest
+                    <li class="nav-item">
+                        <a href="/login" class="nav-link">Entrar</a>
                     </li>
                     <li class="nav-item">
-                        <a href="/" class="nav-link">Cadastrar</a>
+                        <a href="/register" class="nav-link">Cadastrar</a>
                     </li>
+                    @endguest
                 </ul>
              </div>
             </nav>
