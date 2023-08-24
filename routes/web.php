4346b2d7d49a7182e6b007071e835a9d6c27b048
susysd <susyane.dantas33@gmail.com>
2023-08-24 14:12:33 -0300

    correção

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,5 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/', [EventController::class, 'index']);
 });
